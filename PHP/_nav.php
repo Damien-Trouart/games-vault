6e0b3cc081c4c918dea727dd221fd22d23051952
Damien-Trouart <damien.trouart@gmail.com>
2025-06-05 15:58:26 +0200

    first commit

---
+++ PHP/_nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="../HTML/index.html">Games</a></li>
+        <li><a href="../HTML/discover.html">Discover</a></li>
+        <li><a href="../HTML/profile.html">Profile</a></li>
+    </ul>
+</nav>

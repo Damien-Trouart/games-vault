8a4f1e982b3f2d60ab6fa73c954464ac6e91b962
Damien-Trouart <damien.trouart@gmail.com>
2025-06-25 14:14:20 +0200

    docs:avancée dans le fonctionnement des tags, ajout du bouton clear

---
+++ TS/tags.ts
@@ -0,0 +1,108 @@
+import { Game } from "./interface.js";
+
+/**
+ * Des datas du fichier games.json, création d'un objet stockant le nombre d'occurence de chaque tag.
+ * Création d'une fonction affichant les 5 tags les plus présents dans le json 
+ * Barre de recherche pour trouver un tag spécifique.
+ * système de checkbox sur chaque tag pour les cocher, affiner la sélection et les voir apparants dans la liste.
+ */
+export async function updateTags() {
+    const response = await fetch("./../games.json");
+    if (response.ok) {
+        const games = await response.json() as Game[];
+        //Create an object, tagCounts, to store the count of each tag.
+        const tagsCount: { [key: string]: number } = {};
+
+        games.forEach(game => {
+            //For each game, we navigate through its tags
+            game.tags.forEach((tag: string) => {
+                //for each tag of each game, we add its count to the tagsCount object already created or we initialize it to 1 if it doesn't exist yet (it would be "undefined" otherwise)
+                tagsCount[tag] = (tagsCount[tag] || 0) + 1;
+            });
+        })
+
+
+        //Now that we have the count of each tag in the tagsCount object, we can sort it to get the top 5 most used tags.
+        const tagsCountEntries: [string, number][] = Object.entries(tagsCount).sort((a, b) => {
+            return b[1] - a[1];
+        });
+        const topTags: [string, number][] = tagsCountEntries.slice(0, 5)
+        const topTagsName: string[] = topTags.map(([tag]) => {
+            return tag
+        })
+
+        //Now the aim is to create a function that displays specific tags, these tags being attribute of the function
+        //(that's why we created topTagsName, let's have it as attribute of the function to display the top 5 tags)
+        const tagList: HTMLUListElement | null = document.querySelector('.tagList');
+        //Création d'un set stockant les tags séléctionnés pour éviter d'afficher des doublons.
+        const selectedTags: Set<string> = new Set()
+
+        /**
+         * Permet de créer une liste de tag à afficher dans l'<ul>
+         * @param tagsInDisplay Tableau de nom de tags à afficher
+        */
+        function createTagList(tagsInDisplay: string[]) {
+            //On fusionne les tags séléctionnés que l'on fusionne avec les tags à afficher sans doublons grâce au set.
+            const allTagsToShow = Array.from(new Set([...selectedTags, ...tagsInDisplay]));
+            tagList!.innerHTML = "";
+            allTagsToShow.forEach((tag) => {
+                tagList!.innerHTML += `
+                <li>
+                        <label>
+                            <input type="checkbox" name="tag" class="tag" value="${tag}" />
+                            ${tag} <span class="tagCount">(${tagsCount[tag] || 0})</span>
+                        </label>
+                    </li>
+                `
+            });
+            const generatedTags: HTMLInputElement[] = Array.from(document.querySelectorAll('.tag'));
+            generatedTags.forEach((generatedTag: HTMLInputElement) => {
+                //On vérifie si le tag est déjà séléctionné dans le set, auquel cas on le coche.
+                if(selectedTags.has(generatedTag.value)) {
+                    generatedTag.checked = true;
+                }
+                // generatedTag.checked = selectedTags.has(generatedTag.value)
+                generatedTag.addEventListener('change', () => {
+                    //si le tag est cohé, on l'ajoute au set des tags sélectionnés
+                    if (generatedTag.checked) {
+                        selectedTags.add(generatedTag.value);
+
+                    }
+                    else if (!generatedTag.checked) {
+                        //sinon, on le retire d e ce set.
+                        selectedTags.delete(generatedTag.value);
+                    }
+                    //on met à jour la liste des tags affichés
+                    TagSearchResult();
+                })
+            });
+        }
+        createTagList(topTagsName);
+
+        const tagSearchInput: HTMLInputElement | null = document.querySelector('.tagSearch-input');
+        const tagSearchBtn: HTMLButtonElement|null = document.querySelector('.tagSearch-btn');
+        tagSearchInput?.addEventListener('input', TagSearchResult);
+        tagSearchBtn?.addEventListener('click',() => {
+            tagSearchInput!.value = "";
+            createTagList(topTagsName);
+        })
+        
+        /**
+         * Permet de retourner:
+         *  -   soit une liste des 5 tags les plus présents dans le json si la recherche est <= 1 charactère
+         *  -   soit une liste de tags filtrée par rapport à la recherche de l'utilisateur.
+         */
+        function TagSearchResult() {
+            const searchCleaned: string = tagSearchInput!.value.trim().toLowerCase();
+            if (searchCleaned.length <= 1) {
+                createTagList(topTagsName);
+            } else {
+                const filteredSearch: string[] = Object.keys(tagsCount).filter((tag) => {
+                    return tag.toLowerCase().includes(searchCleaned);
+                });
+                createTagList(filteredSearch);
+            }
+        };
+        TagSearchResult()
+    }
+}
